22ff0c4e5a03795c4b1c60dcf3395e9f0ffe78c7
Will Newman <wnewman88@gmail.com>
2017-01-18 09:35:18 -0500

    Updated ui controller for about and goals pages

--- mapofinnovation/controllers/uifunc.py
+++ mapofinnovation/controllers/uifunc.py
@@ -16,39 +16,46 @@ class UifuncController(BaseController):
     def index(self):
         # Return a rendered front page  template
         markers = []
-	indices = {
-		"name": "name",
-		"city": "city",
-		"country": "country",
-		"website": "primary_website",
-		"primarytype": "primary_type",
-		"multitypes": "types_multiple",
-		"description": "description",
-		"latitude": "latitude",
-		"longitude":"longitude",
-		"services": "services"
-	}
-	r = redis.Redis("localhost")
-	i = 0 
-	for key in r.scan_iter():
-		marker = {}
-		row = r.hgetall(key)
-		for header in indices.keys():
-			marker[header] = unicode(row[str(indices[header])], errors='replace')
-		markers.append(marker)
-	c.markers = json.dumps(markers)		
-	return render('/makermap.html')
+    	indices = {
+    		"name": "name",
+    		"city": "city",
+    		"country": "country",
+    		"website": "primary_website",
+    		"primarytype": "primary_type",
+    		"multitypes": "types_multiple",
+    		"description": "description",
+    		"latitude": "latitude",
+    		"longitude":"longitude",
+    		"services": "services"
+    	}
+    	r = redis.Redis("localhost")
+    	i = 0 
+    	for key in r.scan_iter():
+    		marker = {}
+    		row = r.hgetall(key)
+    	
+        	for header in indices.keys():
+    			marker[header] = unicode(row[str(indices[header])], errors='replace')
+		
+        markers.append(marker)
+    	c.markers = json.dumps(markers)		
+    	return render('/makermap.html')
 
     def wikipage(self,id=None):
-	#Return a wiki for the given space 
-	r = redis.Redis('localhost')
-	if id is None :
-		return 'Provide a valid space id'
- 	elif r.exists(id):
-		data = r.hgetall(id)
-		addresstext = str(data['street_address']).decode("ISO-8859-1")
-		websitetext = urllib.unquote(data['primary_website']).decode('utf8')
-		return render('/wikipage.html',extra_vars={'last_updated':str(data['last_updated']),'name':str(data['name']),'status':str(data['status']),'website_url':websitetext,'primarytype':str(data['primary_type']),'secondarytype':'','space_description':str(data['description']),'address':addresstext})
-	else :
-		return 'There is no space with this id. Please recheck and submit'
+        #Return a wiki for the given space 
+        r = redis.Redis('localhost')
+        if id is None :
+            return 'Provide a valid space id'
+        elif r.exists(id):
+            data = r.hgetall(id)
+            addresstext = str(data['street_address']).decode("ISO-8859-1")
+            websitetext = urllib.unquote(data['primary_website']).decode('utf8')
+            return render('/wikipage.html',extra_vars={'last_updated':str(data['last_updated']),'name':str(data['name']),'status':str(data['status']),'website_url':websitetext,'primarytype':str(data['primary_type']),'secondarytype':'','space_description':str(data['description']),'address':addresstext})
+        else :
+            return 'There is no space with this id. Please recheck and submit'
 
+    def about(self):
+        return render('/about.html')		
+    
+    def goals(self):
+        return render('/goals.html')
